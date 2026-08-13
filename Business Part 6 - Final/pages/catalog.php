58e9a11f3c13e47d2f6edec2d4a3cb11a535b6db
Nicolas Sabbagha <94149575+nickster369@users.noreply.github.com>
2026-08-13 12:15:07 -0300

    Update catalog.php

--- Business Part 6 - Final/pages/catalog.php
+++ Business Part 6 - Final/pages/catalog.php
@@ -1,15 +1,14 @@
-<!--catalog.php-->
 <?php
 session_start();
-include '/home/course/u31/public_html/submissions/test/common/document_head.html';
+include __DIR__ . '/../common/document_head.html';
 ?>
 
 <body class="body w3-auto">
     <header class="w3-black">
         <?php
-        include '/home/course/u31/public_html/submissions/test/common/banner.php';
-        include '/home/course/u31/public_html/submissions/test/common/menus.html';
-        include '/home/course/u31/public_html/submissions/test/scripts/connectToDatabase.php';
+        include __DIR__ . '/../common/banner.php';
+        include __DIR__ . '/../common/menus.html';
+        include __DIR__ . '/../scripts/connectToDatabase.php';
         ?>
     </header>
     <main class="w3-container w3-border-black w3-light-grey w3-border-top w3-border-bottom">
@@ -17,11 +16,11 @@ include '/home/course/u31/public_html/submissions/test/common/document_head.html
             <article>
                 <h2 class="w3-center">Product Catalog</h2>
                 <?php
-                include '/home/course/u31/public_html/submissions/test/scripts/catalogProcess.php';
+                include __DIR__ . '/../scripts/catalogProcess.php';
                 ?>
             </article>
         </div>
     </main>
     <?php
-    include '/home/course/u31/public_html/submissions/test/common/footer.html';
+    include __DIR__ . '/../common/footer.html';
     ?>
