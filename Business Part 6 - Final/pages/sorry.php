06ee1177946ee75172d2d1de82c4e92edbd38163
Nicolas Sabbagha <94149575+nickster369@users.noreply.github.com>
2026-08-13 12:10:15 -0300

    Update sorry.php

--- Business Part 6 - Final/pages/sorry.php
+++ Business Part 6 - Final/pages/sorry.php
@@ -1,14 +1,13 @@
-<!--sorry.php-->
 <?php
 session_start();
-include '/home/course/u31/public_html/submissions/test/common/document_head.html';
+include __DIR__ . '/../common/document_head.html';
 ?>
 
 <body class="body w3-auto">
     <header class="w3-black">
         <?php
-        include '/home/course/u31/public_html/submissions/test/common/banner.php';
-        include '/home/course/u31/public_html/submissions/test/common/menus.html';
+        include __DIR__ . '/../common/banner.php';
+        include __DIR__ . '/../common/menus.html';
         ?>
     </header>
     <main class="w3-container w3-border-black w3-light-grey w3-border-top w3-border-bottom">
@@ -23,5 +22,5 @@ include '/home/course/u31/public_html/submissions/test/common/document_head.html
         </div>
     </main>
     <?php
-    include '/home/course/u31/public_html/submissions/test/common/footer.html';
+    include __DIR__ . '/../common/footer.html';
     ?>
