183638a58c609efc2c9f94b04cf532d07dcba17b
Nicolas Sabbagha <94149575+nickster369@users.noreply.github.com>
2026-08-13 12:15:19 -0300

    Update category.php

--- Business Part 6 - Final/pages/category.php
+++ Business Part 6 - Final/pages/category.php
@@ -1,15 +1,14 @@
-<!--category.php-->
 <?php
 session_start();
-include '/home/course/u31/public_html/submissions/test/common/document_head.html';
+include __DIR__ . '/../common/document_head.html';
 ?>
 
 <body class="body w3-auto" onload="carousel()">
     <header class="w3-black">
         <?php
-        include '/home/course/u31/public_html/submissions/test/common/banner.php';
-        include '/home/course/u31/public_html/submissions/test/common/menus.html';
-        include '/home/course/u31/public_html/submissions/test/scripts/connectToDatabase.php';
+        include __DIR__ . '/../common/banner.php';
+        include __DIR__ . '/../common/menus.html';
+        include __DIR__ . '/../scripts/connectToDatabase.php';
         ?>
     </header>
     <main class="w3-container w3-border-black w3-light-grey w3-border-top w3-border-bottom">
@@ -18,14 +17,14 @@ include '/home/course/u31/public_html/submissions/test/common/document_head.html
                 <h4>Complete List of
                     Product Catagories</h4>
                 <?php
-                include '/home/course/u31/public_html/submissions/test/scripts/categoryProcess.php';
+                include __DIR__ . '/../scripts/categoryProcess.php';
                 ?>
             </article>
         </div>
         <?php
-        include '/home/course/u31/public_html/submissions/test/resources/images_and_labels_product_category.html';
+        include __DIR__ . '/../resources/images_and_labels_product_category.html';
         ?>
     </main>
     <?php
-    include '/home/course/u31/public_html/submissions/test/common/footer.html';
+    include __DIR__ . '/../common/footer.html';
     ?>
